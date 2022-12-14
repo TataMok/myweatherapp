c03e6bc57d66493d735742899ef8bedb6be7cada
TataMok <114225623+TataMok@users.noreply.github.com>
2022-12-14 12:24:37 +0200

    change function displayTemp

--- src/script.js
+++ src/script.js
@@ -23,26 +23,29 @@ function search(event) {
 let form = document.querySelector(".search-form");
 form.addEventListener("submit", search);
 
-function current(event) {
-  event.preventDefault();
-  let currentCity = document.querySelector("#current-location-button");
-  alert`${currentCity}`;
-}
-let button = document.querySelector(".current-location");
-button.addEventListener("current");
-
-function showTemperature(response) {
-  console.log(response.data);
+function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
-  let description = document.querySelector("#description");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
+  let iconElement = document.querySelector("#icon");
+
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
   cityElement.innerHTML = response.data.name;
-  description.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatData(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://opnenweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-let units = "metric";
 let city = "New York";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=¢{position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(showTemperature);
